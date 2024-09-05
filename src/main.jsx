1435487318f4b01d77332cb16c329ee6253be1ea
Franco Ursino <franncoursinno@gmail.com>
2024-09-05 13:48:33 -0300

    [ADD] chatbot

--- src/main.jsx
+++ src/main.jsx
@@ -8,21 +8,23 @@ import { NextUIProvider } from "@nextui-org/react";
 import Footer from './components/Footer.jsx'
 import { AuthProvider } from './components/DB/AuthContext.jsx'
 import { UserProvider } from './components/userProfile/UserContext.jsx'
+import HelperBot from './components/ChatBot.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <BrowserRouter>
       <NextUIProvider>
         <AuthProvider>
           <UserProvider>
+            
             <Navbar />
             <main className='dark bg-[#161616]'>
               <App />
+              chat
             </main>
+            <HelperBot/>
             <Footer />
           </UserProvider>
         </AuthProvider>
       </NextUIProvider>
-    </BrowserRouter>
-  </StrictMode>,
+    </BrowserRouter>,
 )
